880b9f620ef411c8f29e8e645821e054e0a3369c
ItelSunday <iteldomingotech@gmail.com>
2019-02-15 11:47:27 -0800

    added styling

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,12 +3,17 @@ import React from 'react';
 function Character(props) {
     return (
         <div className='character'>
-            <h3>{props.character.name}</h3>
+            <h2>{props.character.name}</h2>
+            <p>Birth Year: {props.character.birth_year}</p>
             <p>Gender: {props.character.gender}</p>
             <p>Height: {props.character.height} CM</p>
             <p>Skin Color: {props.character.skin_color}</p>
             <p>Hair Color: {props.character.hair_color}</p>
             <p>Eye Color: {props.character.eye_color}</p>
+            
+            
+
+
             
         </div>
     )
